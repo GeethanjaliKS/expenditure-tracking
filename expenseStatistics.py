b9e333f72758bf655f4f078c083bd715b5ff7e01
saji37 <sajiththomas162631@gmail.com>
2023-01-12 10:46:57 +0530

    categoryAnalytics added

---
+++ expenseStatistics.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import pymongo
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["Expense_Trackerdb"]
+
+mycol = mydb["expense"]
+def categoryExpense():
+
+  d=mycol.find({},{"Category":1,"Amount":1,"_id":0})
+  key=[]
+  data=[]
+  for i in d:
+    key.append(i['Category'])
+    data.append(i['Amount'])
+#   print(key)
+#   print(data)
+  df = pd.DataFrame({'Category': key,'Amount': data}, columns=['Category', 'Amount'])
+#   print(df)
+  res=df.groupby('Category').sum().reset_index()
+  print('----------------------------------------------------')
+  print('EXPENSE BY CATEGORIES')
+  print('----------------------------------------------------')
+#   print(res.mode())
+  print(res)
+categoryExpense()
+
+
+
+
+def highestExpense():
+  d=mycol.find({},{"Category":1,"Amount":1,"_id":0})
+  key=[]
+  data=[]
+  for i in d:
+    key.append(i['Category'])
+    data.append(i['Amount'])
+#   print(key)
+#   print(data)
+  df = pd.DataFrame({'Category': key,'Amount': data}, columns=['Category', 'Amount'])
+#   print(df)
+  res=df.groupby('Category').sum().reset_index()
+#   print(res.mode())
+  value=res.query('Amount == Amount.max()')
+  print('----------------------------------------------------')
+  print('MOST EXPENSE')
+  print('----------------------------------------------------')
+  print(value)
+
+highestExpense()
+
+
+
+
