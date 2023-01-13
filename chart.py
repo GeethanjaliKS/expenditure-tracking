75841b2081c1f6ee486675b3b8ae6919bba23e30
GeethanjaliKS <geethanjaliks001@gmail.com>
2023-01-13 22:13:45 +0530

    chart

--- chart.py
+++ chart.py
@@ -1,5 +1,3 @@
-import dataclasses
-import keyword
 import pandas as pd
 import pymongo
 import matplotlib.pyplot as plt
@@ -18,9 +16,23 @@ def categoryExpense():
   for i in d:
     key.append(i['Category'])
     data.append(i['Amount'])
-plt.hist(key,data)
-plt.ylabel("category")
-plt.xlabel("Expense")
-plt.show()
+    df = pd.DataFrame({'Category': key,'Amount': data}, columns=['Category', 'Amount'])
+    res=df.groupby('Category').sum().reset_index()
+  # print(res.Category)
+  col_cat=list(res["Category"])
+  print(col_cat) 
+  col_amt=list(res["Amount"])
+  print(col_amt) 
+  # x=[key]
+  # y=[data]  
+  
+   
+  y_pos=np.arange(len(col_amt))
+  plt.bar(y_pos,col_amt)
+  plt.xticks(y_pos,col_cat)
+  plt.xlabel("category")
+  plt.ylabel("Expense")
+  # plt.xticks(x,key,color='red',fontweight='bold',fontsize='17' )
+  plt.show()
 categoryExpense()    
     
