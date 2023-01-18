e9be1284facc561e70446fef94624f639e60cce6
GeethanjaliKS <geethanjaliks001@gmail.com>
2023-01-18 21:54:27 +0530

    Update expenseTracker.py

--- expenseTracker.py
+++ expenseTracker.py
@@ -7,12 +7,11 @@ import chart
 
 
 
-# res1=db.CurrentYear()
-res1=chart.categoryExpense()
+res1=db.CurrentYear()
 res2=month.currentMonth()
 res3=cdate.currentDate()
 a=[res1,res2,res3]
-myvar = pd.Series(a, index = ["categoryExpense", "CurrentMonth", "CurrentDate"])
+myvar = pd.Series(a, index = ["CurrentYear", "CurrentMonth", "CurrentDate"])
 print("==========Year,Month and Day Expense==========")
 print(myvar)
 print("==============================================")
