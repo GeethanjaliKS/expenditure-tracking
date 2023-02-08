ebdc9cd476f26b53ae5936ff16101d32fbab008a
GeethanjaliKS <geethanjaliks001@gmail.com>
2023-02-08 13:37:29 +0530

    search

---
+++ Search.py
@@ -0,0 +1,13 @@
+
+# import pandas as pd
+# import pymongo
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["Expense_Trackerdb"]
+
+# mycol = mydb["expense"]
+
+# # while True:
+# print("Search particular Expense")
+# ch=expense.find({"Category":"Medical"})
+
+# print(ch)
