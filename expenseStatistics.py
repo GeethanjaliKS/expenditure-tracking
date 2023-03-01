bbc0ba97eddcababe9c4f53086af5843c2f501fa
saji37 <sajiththomas162631@gmail.com>
2023-03-01 16:15:37 +0530

    report

--- expenseStatistics.py
+++ expenseStatistics.py
@@ -4,26 +4,25 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["Expense_Trackerdb"]
 
 mycol = mydb["expense"]
-def categoryExpense():
+# def categoryExpense():
 
-  d=mycol.find({},{"Category":1,"Amount":1,"_id":0})
-  key=[]
-  data=[]
-  for i in d:
-    key.append(i['Category'])
-    data.append(i['Amount'])
-  # print(key)
-  # print(data)
-  df = pd.DataFrame({'Category': key,'Amount': data}, columns=['Category', 'Amount'])
-#   print(df)
-  res=df.groupby('Category').sum().reset_index()
-#   print('----------------------------------------------------')
-#   print('EXPENSE BY CATEGORIES')
-#   print('----------------------------------------------------')
-# #   print(res.mode())
-  # print(res)
-  return res
-categoryExpense()
+#   d=mycol.find({},{"Category":1,"Amount":1,"_id":0})
+#   key=[]
+#   data=[]
+#   for i in d:
+#     key.append(i['Category'])
+#     data.append(i['Amount'])
+#   # print(key)
+#   # print(data)
+#   df = pd.DataFrame({'Category': key,'Amount': data}, columns=['Category', 'Amount'])
+# #   print(df)
+#   res=df.groupby('Category').sum().reset_index()
+# #   print('----------------------------------------------------')
+# #   print('EXPENSE BY CATEGORIES')
+# #   print('----------------------------------------------------')
+# # #   print(res.mode())
+#   # print(res)
+#   return res
 
 
 
